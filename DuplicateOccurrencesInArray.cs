e829e3886a50847e9fd8ed49d9c1c5938c7787b7
pkwayfair <38385272+pkwayfair@users.noreply.github.com>
2024-02-23 14:35:52 -0500

    Add files via upload

---
+++ DuplicateOccurrencesInArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODemo
+{
+    public class DuplicateOccurrencesInArray
+    {
+        public Dictionary<int,int> CountOccurrences(int[] array)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (result.ContainsKey(array[i]))
+                {
+                    result[array[i]]++;
+                } else
+                {
+                    result.Add(array[i], 1);
+                }
+            }
+            return result;
+        }
+    }
+}
