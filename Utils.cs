835ac0e0c948224a4999611fdcc231b57d3fd21e
pkwayfair <38385272+pkwayfair@users.noreply.github.com>
2024-02-23 15:55:07 -0500

    Update Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -33,5 +33,21 @@ namespace OODemo
             }
             return result;
         }
+
+        public static List<int> GenerateFibSeries(int target)
+        {
+            List<int> result = new List<int>();
+            int firstNumber = 1, secondNumber = 2;
+            result.Add(firstNumber);
+            result.Add(secondNumber);
+            for (int i=0;i< target; i++)
+            {
+                var x = firstNumber + secondNumber;
+                result.Add(x);
+                firstNumber = secondNumber;
+                secondNumber = x;
+            }
+            return result;
+        }
     }
 }
