64a9d3955abb20cc120f5d0eef0af7693d26c534
pkwayfair <38385272+pkwayfair@users.noreply.github.com>
2024-02-23 15:10:00 -0500

    Add files via upload

---
+++ Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODemo
+{
+    public static class Utils
+    {
+        public static List<int> GeneratePrimeNumbers(int target)
+        {
+            List<int> result = new List<int>();
+            int i = 1;
+            while(i <= target) {
+                if (i == 1)
+                {
+                    result.Add(i);
+                } 
+                else
+                {
+                    int counter = 0;
+                    for (int j = 1; j < i; j++)
+                    {
+                        if (i%j == 0)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter <=1) { result.Add(i); }
+                }
+                i += 1;
+            }
+            return result;
+        }
+    }
+}
