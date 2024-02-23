7203698448504867cf807e8af2ba7489bef4d73f
pkwayfair <38385272+pkwayfair@users.noreply.github.com>
2024-02-23 14:14:40 -0500

    Add files via upload

---
+++ ArrayCodeChallenges.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODemo
+{
+    public class ArrayCodeChallenges
+    {
+        public int GetSmallestPositiveInteger(int[] arr)
+        {
+            int i = 1;
+            while (true)
+            {
+                if (!arr.Contains(i))
+                {
+                    break;
+                }
+                i++;
+            }
+            return i;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using OODemo;
+
+ArrayCodeChallenges arrayCodeChallenges = new ArrayCodeChallenges();
+// pass an array to the function that returns the smallest 
+// positive integer that is not present in the array
+
+
+Console.WriteLine($"The smallest positive integer not present in the " +
+    $"array is {arrayCodeChallenges.GetSmallestPositiveInteger([3, 6, 4, 8, 9, 6, 3, 2, 1])}");
